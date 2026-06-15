3fc8ee490eb32edc08cff114a8ab979479458971
DTuan <doantuan1625@gmail.com>
2026-06-16 00:46:16 +0700

    add(stm32f103xx.h): add common STM32F103 header file

---
+++ application/driver/stm32f103/stm32f103xx.h
@@ -0,0 +1,17 @@
+#ifndef STM32F103xx_H
+#define STM32F103xx_H
+
+#include <stdint.h>
+
+#include "../../core/nvic/nvic.h"
+#include "../../core/scb/scb.h"
+
+#include "../../driver/stm32f103/Inc/gpio.h"
+#include "../../driver/stm32f103/Inc/rcc.h"
+#include "../../driver/stm32f103/Inc/timer.h"
+#include "../../driver/stm32f103/Inc/usart.h"
+#include "../../driver/stm32f103/Inc/adc.h"
+#include "../../driver/stm32f103/Inc/dma.h"
+#include "../../driver/stm32f103/Inc/exti.h"
+
+#endif // STM32F103xx_H
